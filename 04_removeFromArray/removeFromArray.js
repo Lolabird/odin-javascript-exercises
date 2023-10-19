f479d02f961232fe999aae8474ae082820cced0a
lolabird <octavia@octavianichols.com>
2023-10-19 17:56:18 -0600

    Add rest parameters

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,18 @@
-const removeFromArray = function(someArray, item) {
-    for (i = 0; i < someArray.length; i++) {
-        if (someArray[i] === item) {
-            someArray.splice(i, 1);
-
+const removeFromArray = function(someArray, ...items) {
+    console.log(items);
+    
+    for (i = 0; i < items.length; i++) {
+        for (j = 0; j < someArray.length; j++) {
+            if (someArray[j] === items[i]) {
+                someArray.splice(j, 1);
+            }
         }
     }
 
     return someArray;
 };
 
-console.log(removeFromArray([1, 2, 3, 4, 3], 3));
+console.log(removeFromArray([1, 2, 3, 4, 3], 3, 4));
 
 // Do not edit below this line
 module.exports = removeFromArray;
