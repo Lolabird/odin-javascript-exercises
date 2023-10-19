160e791db5a1be09cecbd318f9cac12247057586
lolabird <octavia@octavianichols.com>
2023-10-19 16:12:44 -0600

    Solve ReverseString, get rid of test scips in spec

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(oldStr) {
+    newStr = '';
+    strLength = oldStr.length;
 
+    for (i = 0; i < strLength; i++) {
+        newStr = oldStr.slice(i, i+1) + newStr;
+    }
+
+    return newStr;
 };
+
+console.log(reverseString('hello there'))
 
 // Do not edit below this line
 module.exports = reverseString;
