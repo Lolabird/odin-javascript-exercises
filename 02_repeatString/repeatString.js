421cb49093497ce44110ae8566c6accfc6addd76
lolabird <octavia@octavianichols.com>
2023-10-19 15:35:01 -0600

    Add initial solution for repeatString.js, remove skips for spec, edit .gitignore

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(oldStr, num) {
+    
+    newStr = '';
 
+    for(i = 0; i < num; i++){
+        newStr += oldStr;
+    }
+
+    return newStr;
 };
+
+console.log(repeatString('hey', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
