319608f0f98537e09e18cbf1dd9bdda2bb40d4b0
lolabird <octavia@octavianichols.com>
2023-10-19 17:18:20 -0600

    Solve removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(someArray, item) {
+    for (i = 0; i < someArray.length; i++) {
+        if (someArray[i] === item) {
+            someArray.splice(i, 1);
 
+        }
+    }
+
+    return someArray;
 };
+
+console.log(removeFromArray([1, 2, 3, 4, 3], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
