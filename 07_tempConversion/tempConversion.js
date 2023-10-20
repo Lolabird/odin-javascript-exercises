d093b8beccdc80731377d5becb0467a0621ff5e6
lolabird <octavia@octavianichols.com>
2023-10-19 19:25:22 -0600

    Solve tempConversion and round to one decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celcius = (5/9) * (fahrenheit - 32);
+
+  return +(celcius.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = (9/5) * celcius + 32;
+
+  return +(fahrenheit.toFixed(1))
 };
 
 // Do not edit below this line
