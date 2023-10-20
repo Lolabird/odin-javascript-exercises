83d7b8909aa847641a135c20e7d87fefdc4c09ff
lolabird <octavia@octavianichols.com>
2023-10-19 18:08:49 -0600

    Complete initial solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
+const sumAll = function(begin, end) {
+    totalSum = 0;
+
+    for (i = begin; i <= end; i++) {
+        totalSum += i;
+    }
+
+    return totalSum;
 
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
