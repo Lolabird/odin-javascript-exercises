2a41466a3c3e25c946e5913a508a314a0483ceb8
lolabird <octavia@octavianichols.com>
2023-12-14 12:43:02 -0700

    Complete and pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,12 +1,18 @@
 const palindromes = function (str) {
+    newStr = str.toLowerCase();
+    newerStr = ""
     revStr = "";
     pal = false;
 
-    for (let char of str) {
-        revStr = char + revStr;
+
+    for (let char of newStr) {
+        if (char.match(/[a-zA-Z0-9]/i)){
+            newerStr += char;
+            revStr = char + revStr;
+        }
     }
 
-    if (str == revStr) {
+    if (newerStr == revStr) {
         pal = true;
     }
     console.log(revStr);
