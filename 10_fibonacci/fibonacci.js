b0e5fe2ae8f8e9ff02b5287846dd21d72749231a
lolabird <octavia@octavianichols.com>
2023-12-14 16:02:15 -0700

    Pass all 'nth fibonacci number is x' tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fn1 = 0;
+    let fn2 = 0;
+    let fib = 0
 
-};
+    for (let i= 0; i <= num; i++) {
+        if (i === 0) {
+            fib = 0;
+        } else if (i === 1) {
+            fib = 1;
+        } else {
+            fn2 = fn1;
+            fn1 = fib;
+            fib = fn1 + fn2;
+        }
+    }
+    
+    return fib;
+}; 
 
 // Do not edit below this line
 module.exports = fibonacci;
