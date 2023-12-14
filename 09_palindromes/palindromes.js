f6c8941689a92256964aeed0bde04c0991e202a0
lolabird <octavia@octavianichols.com>
2023-12-14 12:06:01 -0700

    Pass single word test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    revStr = "";
+    pal = false;
 
+    for (let char in str) {
+        revStr = str[char] + revStr;
+    }
+
+    if (str == revStr) {
+        pal = true;
+    }
+    console.log(revStr);
+    return pal;
 };
 
 // Do not edit below this line
