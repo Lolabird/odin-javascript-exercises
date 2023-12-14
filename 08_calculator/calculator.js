7c793302b732f6fa96d5c0a556f8f16bbb8181c5
lolabird <octavia@octavianichols.com>
2023-12-14 10:56:26 -0700

    Complete add, subtract, and sum and pass related tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let arraySum = 0;
+  
+  for (i = 0; i < array.length; i++) {
+    arraySum += array[i];
+  }
+
+	return arraySum;
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  return a * b;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	return factorial(a);
 };
 
 // Do not edit below this line
