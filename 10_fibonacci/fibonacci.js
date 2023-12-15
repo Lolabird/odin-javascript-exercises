c7ffb423861d08d48b7a4f62e48ab15baba314ed
lolabird <octavia@octavianichols.com>
2023-12-14 17:03:34 -0700

    Complete and pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,21 +1,36 @@
 const fibonacci = function(num) {
     let fn1 = 0;
     let fn2 = 0;
-    let fib = 0
+    let fib = 0;
 
-    for (let i= 0; i <= num; i++) {
-        if (i === 0) {
-            fib = 0;
-        } else if (i === 1) {
-            fib = 1;
+    try {
+        newNum = parseInt(num);
+
+        if (isNaN(newNum)) {
+            throw new Error(`"${num}" is not a number.`)
         } else {
-            fn2 = fn1;
-            fn1 = fib;
-            fib = fn1 + fn2;
+            if (newNum < 0 ) {
+                return "OOPS";
+            } else {
+                for (let i= 0; i <= newNum; i++) {
+                    if (i === 0) {
+                        fib = 0;
+                    } else if (i === 1) {
+                        fib = 1;
+                    } else {
+                        fn2 = fn1;
+                        fn1 = fib;
+                        fib = fn1 + fn2;
+                    }
+                }
+        
+                 return fib;
+            }
         }
+    } catch (error) {
+        return error;
     }
     
-    return fib;
 }; 
 
 // Do not edit below this line
