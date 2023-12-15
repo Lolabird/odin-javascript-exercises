ec374bd8d3b76344201364921233b831fb7a157c
lolabird <octavia@octavianichols.com>
2023-12-14 18:41:19 -0700

    Complete exercise and pass tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
 
+    let peoples = [];
+    peoples = peoples.concat(array);
+
+    peoples.forEach(person => {
+        if (!person.yearOfDeath) {
+            person.age = currentYear - person.yearOfBirth;
+        } else {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+    });
+
+    peoples.sort((a, b) => b.age - a.age);
+    return peoples[0];
 };
 
 // Do not edit below this line
